5fff803a78bbde34455dc909469cbae67bd5af83
Dean Ayalon <deanayalon@me.com>
2024-11-11 18:38:32 +0200

    init

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+
+    // integer (whole number)
+    int age = 21;
+    int year = 2023;
+    int days = 3;
+
+    // double (decimal)
+    double (price) = (10.99);
+    double gpa = 2.5;
+    double temperature = 25.1;
+
+    std::cout << price;
+
+    return 0;
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+[Source](https://discord.com/channels/470167643710816277/1107255712628871229/1305566415419605085)
+
+# Problem
+OP's program seems to print '5' instead of '10.99'
+> Could not replicate the error, I get 10.99 properly
