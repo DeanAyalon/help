81afa6dcb169195457523bc08250716166c06be9
Dean Ayalon <deanayalon@me.com>
2025-04-16 15:58:51 +0300

    Fix errors

--- readme.md
+++ readme.md
@@ -2,3 +2,9 @@
 
 # Request
 Evaluate the correctness and efficiency of their code
+
+# Evaluation
+## Errors 
+- one-too-many `}`, after closing the second `while` loop?
+- Undeclared `top` variable, meant to be `temp.top()`?
+- By the time OP attempts to push elements back into `s`, both stacks are depleted, and the program crashes
--- stack.cpp
+++ stack.cpp
@@ -13,10 +13,23 @@ void displayReversed(std::stack<int> s) {
     // Display reversed elements
     while (!temp.empty()) {
         std::cout << temp.top() << std::endl;
+        s.push(temp.top());
         temp.pop();
     }
-}
 
     // Optional: Push the element back if we want to restore the stack
-    s.push(top);
+    std::cout << "s.size: " << s.size() << std::endl;
+}
+
+int main() {
+    std::stack<int> s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    s.push(7);
+    s.push(5);
+    s.push(9);
+    displayReversed(s);
+    return 0;
 }
